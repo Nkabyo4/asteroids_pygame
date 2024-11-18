acd31cd37863755d513fee4b7dfdb0329f2b0578
Nkabyo4 <nkabyo13@gmail.com>
2024-11-18 14:07:29 -0600

    Added player.py file / modified main.py + constants.py files

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time_clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = (SCREEN_WIDTH / 2), y = (SCREEN_HEIGHT / 2))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = time_clock.tick(60) / 1000
         print (dt)
