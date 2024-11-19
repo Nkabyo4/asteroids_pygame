8c9e78269bdb48ceab06d117c54fbcfa0e1fb15f
Nkabyo4 <nkabyo13@gmail.com>
2024-11-19 14:35:52 -0600

    Added rate limit for shots / updated constants.py and player.py

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -44,6 +45,8 @@ class Player(CircleShape):
         
         if keys[pygame.K_SPACE]:
             self.shoot()
+        
+        self.timer -= dt
     
     def move(self, dt):
 
@@ -52,5 +55,8 @@ class Player(CircleShape):
     
     def shoot(self):
 
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
